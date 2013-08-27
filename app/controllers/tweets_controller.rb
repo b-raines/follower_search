ed3ca8e3b65a39f013fa6460baf09722436b512f
Brent Raines <bt.raines@gmail.com>
2013-08-26 20:51:14 -0400

    Only query new tweets

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,12 +1,16 @@
 class TweetsController < ApplicationController
 
+  before_action :get_followers
+
   def new
-    @followers = {}
-    current_user.followers.each do |follower|
-      @followers[follower.id] = follower.name
-    end
+    
     @followers.each do |id, name|
-      Twitter.user_timeline(name).each do |status|
+      current_tweets = Follower.find(id).tweets
+      tweet_ids = Array.new(2, "1")
+      current_tweets.each do |tweet|
+        tweet_ids << tweet.tid
+      end
+      Twitter.user_timeline(name, since_id: tweet_ids.max).each do |status|
         Tweet.find_or_create_by_tid(
           tid: status.id,
           content: status.full_text,
@@ -15,6 +19,13 @@ class TweetsController < ApplicationController
       end
     end
 
+  end
+
+  def get_followers
+    @followers = {}
+    current_user.followers.each do |follower|
+      @followers[follower.id] = follower.name
+    end
   end
 
 end
