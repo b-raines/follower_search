aa6c18ce85dd1938b9e10aa828b338a8dc46b660
Brent Raines <bt.raines@gmail.com>
2013-08-28 10:19:50 -0400

    Delete tweets older than 7 days

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,6 +1,7 @@
 class TweetsController < ApplicationController
 
   before_action :get_followers
+  before_action :clean_up
 
   def new
     
@@ -11,12 +12,23 @@ class TweetsController < ApplicationController
         tweet_ids << tweet.tid
       end
       max_id = tweet_ids.max
-      Twitter.user_timeline(name, since_id: max_id).each do |status|
-        Tweet.create(
-          tid: status.id.to_s,
-          content: status.full_text,
-          follower_id: id
-        )
+      if max_id > "1"
+        statuses = Twitter.search(name, since_id: max_id).attrs[:statuses]
+        statuses.each do |status|
+          Tweet.create(
+            tid: status[:id_str],
+            content: status[:text],
+            follower_id: id
+          )
+        end
+      else
+        Twitter.user_timeline(name).each do |status|
+          Tweet.create(
+            tid: status.id.to_s,
+            content: status.full_text,
+            follower_id: id
+          )
+        end
       end
     end
     redirect_to trends_path
@@ -27,6 +39,10 @@ class TweetsController < ApplicationController
     current_user.followers.each do |follower|
       @followers[follower.id] = follower.name
     end
+  end
+
+  def clean_up
+    Tweet.where("created_at < ?", 7.days.ago).delete_all
   end
 
 end
