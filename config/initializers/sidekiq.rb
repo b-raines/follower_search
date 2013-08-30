908a5e70a03449314c4dde34fddf97e662df46d5
Brent Raines <bt.raines@gmail.com>
2013-08-30 12:52:58 -0400

    add procfile for sidekiq

--- config/initializers/sidekiq.rb
+++
@@ -1,9 +0,0 @@
-Sidekiq.configure_server do |config|
-  config.redis = { url: 'redis://redis.example.com:7372/12', namespace: 'mynamespace' }
-
-  database_url = ENV['DATABASE_URL']
-  if database_url
-    ENV['DATABASE_URL'] = "#{database_url}?pool=25"
-    ActiveRecord::Base.establish_connection
-  end
-end
