f164e6cbfe5977dddc4687a72e4f8891e27e2e39
Brent Raines <bt.raines@gmail.com>
2013-08-31 09:46:30 -0400

    setup unicorn config file for heroku

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,11 +1,23 @@
+worker_processes Integer(ENV["WEB_CONCURRENCY"] || 3)
+timeout 15
+preload_app true
+
 before_fork do |server, worker|
-   @sidekiq_pid ||= spawn("bundle exec sidekiq -c 2")
-end
+  @sidekiq_pid ||= spawn("bundle exec sidekiq -c 2")
+  Signal.trap 'TERM' do
+    puts 'Unicorn master intercepting TERM and sending myself QUIT instead'
+    Process.kill 'QUIT', Process.pid
+  end
 
-if ENV["RAILS_ENV"] == "development"
-  worker_processes 1
-else
-  worker_processes 3
-end
+  defined?(ActiveRecord::Base) and
+    ActiveRecord::Base.connection.disconnect!
+end 
 
-timeout 30
+after_fork do |server, worker|
+  Signal.trap 'TERM' do
+    puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to send QUIT'
+  end
+
+  defined?(ActiveRecord::Base) and
+    ActiveRecord::Base.establish_connection
+end
