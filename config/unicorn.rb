b17bba2e9ffa6e89cda1dd388acc4243f7d79086
Brent Raines <bt.raines@gmail.com>
2013-09-01 17:30:59 -0400

    im an idiot

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,4 +1,4 @@
-worker_processes Integer(ENV["WEB_CONCURRENCY"] || 2)
+worker_processes Integer(ENV["WEB_CONCURRENCY"] || 3)
 timeout 15
 preload_app true
 
@@ -10,7 +10,7 @@ before_fork do |server, worker|
 
   defined?(ActiveRecord::Base) and ActiveRecord::Base.connection.disconnect!
 
-  @sidekiq_pid ||= spawn("bundle exec sidekiq -c 2")
+  @sidekiq_pid ||= spawn("bundle exec sidekiq -c 3")
 end
 
 after_fork do |server, worker|
