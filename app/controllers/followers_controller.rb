c88277e7021be339d957480f4534fe06d610a52a
Brent Raines <bt.raines@gmail.com>
2013-09-01 16:21:31 -0400

    fixed follower cursor and refactoring

--- app/controllers/followers_controller.rb
+++ app/controllers/followers_controller.rb
@@ -5,10 +5,12 @@ class FollowersController < ApplicationController
     until @cursor == "0"
       follower_info = current_user.twitter.followers(cursor: @cursor).attrs
       follower_info[:users].each do |follower|
-        current_user.followers.build(
-          fid: follower[:id_str],
-          name: follower[:screen_name]
-        )
+        unless Follower.find_by(fid: follower[:id_str])
+          current_user.followers.build(
+            fid: follower[:id_str],
+            name: follower[:screen_name]
+          )
+        end
       end
       @cursor = follower_info[:next_cursor_str]
     end
